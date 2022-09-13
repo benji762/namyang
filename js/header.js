4c3f60ea4e3fd304c91150d5b9d64667a2e1a5d8
benji762 <112377860+benji762@users.noreply.github.com>
2022-09-13 17:30:59 +0900

    220913_5

--- js/header.js
+++ js/header.js
@@ -8,11 +8,14 @@ $(window).scroll(function(){
       $('header').removeClass('whitecolor');
       $('header').addClass('fixed');
       $('.btn_nav.close').addClass('scroll_down')
+      $('.mo_btn_nav.close').addClass('scroll_down')
     } //if
     else{ 
       $('header').addClass('whitecolor');
       $('header').removeClass('fixed');
       $('.btn_nav.close').removeClass('scroll_down')
+      $('.mo_btn_nav.close').removeClass('scroll_down')
+
       
     }//else
   }) //$(window).scroll
